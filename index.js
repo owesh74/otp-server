fc7469b686fcf8dab57648752b2392c2b1cb4904
owesh74 <oweshkhan1856@gmail.com>
2025-09-06 15:03:22 +0530

    update server

--- index.js
+++ index.js
@@ -51,4 +51,7 @@ app.post("/verify-otp", (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("🚀 Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
